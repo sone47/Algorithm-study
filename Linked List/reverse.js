550412695d79cf0f2c036d1e6a15ea7c95df6dcc
sone <sone47.china@gmail.com>
2018-10-07 20:59:53 +0800

    add linked-list reverse

---
+++ Linked List/reverse.js
@@ -0,0 +1,35 @@
+/**
+ * 链表反转
+ */
+
+function reverse(head) {
+  let pointer = head;
+  let prev = null;
+  let next = null;
+
+  while(pointer) {
+    next = pointer.next;
+    pointer.next = prev;
+    prev = pointer;
+    pointer = next;
+  }
+  head.next = null;
+
+  return prev;
+}
+
+const head = {
+  data: 1,
+  next: {
+    data: 2,
+    next: {
+      data: 3,
+      next: {
+        data: 4,
+        next: null
+      }
+    }
+  }
+};
+
+console.log(reverse(head));
