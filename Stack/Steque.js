1f63cd72751ece31879f28fe17cab9da43e1bac6
sone <sone47.china@gmail.com>
2018-10-18 23:01:13 +0800

    [add][Steque]

---
+++ Stack/Steque.js
@@ -0,0 +1,54 @@
+/**
+ * steque: 一个以栈为目标的队列
+ */
+
+const Stack = require('./constructor');
+
+class Steque {
+  constructor(capacity) {
+    this.stack = new Stack();
+    this.capacity = capacity;
+    this.size = 0;
+  }
+
+  enqueue(value) {
+    if (this.isFull()) {
+      console.warn('Queue is full.');
+    } else {
+      this.stack.push(value);
+      this.size++;
+    }
+  }
+
+  dequeue() {
+    if (this.isEmpty()) {
+      console.warn('Queue is empty.');
+      return;
+    } else {
+      const tempStack = new Stack();
+      const stack = this.stack;
+
+      while (!stack.isEmpty()) {
+        tempStack.push(stack.pop());
+      }
+
+      const dequeueValue = tempStack.pop();
+      while (!stack.isEmpty()) {
+        stack.push(tempStack.pop());
+      }
+
+      this.size--;
+      return dequeueValue;
+    }
+  }
+
+  isEmpty() {
+    return this.stack.isEmpty();
+  }
+
+  isFull() {
+    return this.size === this.capacity;
+  }
+}
+
+module.exports = Steque;
---
+++ Stack/test/Steque.test.js
@@ -0,0 +1,27 @@
+const Steque = require('../Steque');
+
+const queue = new Steque(5);
+
+test('enqueue', () => {
+  expect(queue.isEmpty()).toBe(true);
+  queue.enqueue(1);
+  queue.enqueue(2);
+  expect(queue.size).toBe(2);
+  expect(queue.isEmpty()).toBe(false);
+});
+
+test('dequeue', () => {
+  queue.enqueue(3);
+  queue.enqueue(4);
+  expect(queue.size).toBe(4);
+  queue.dequeue();
+  expect(queue.size).toBe(3);
+});
+
+test('test full', () => {
+  queue.enqueue(4);
+  queue.enqueue(5);
+  expect(queue.size).toBe(5);
+  queue.enqueue(6);
+  expect(queue.size).toBe(5);
+});
